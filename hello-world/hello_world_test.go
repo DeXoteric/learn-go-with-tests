d6f52cd7982f246344bc6c44420d6eabb0f41a04
DeXoteric <dexoteric@gmail.com>
2025-03-28 20:08:00 +0100

    hello-world-02

--- hello-world/hello_world_test.go
+++ hello-world/hello_world_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Marek")
+	want := "Hello, Marek"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
