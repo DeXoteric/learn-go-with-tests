39f7666b7fbb22ea67aaac27f7ec1d454e83f1fb
DeXoteric <dexoteric@gmail.com>
2025-03-29 13:39:39 +0100

    arrays-and-slices-done

--- 04-arrays-and-slices/sum_test.go
+++ 04-arrays-and-slices/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"slices"
 	"testing"
 )
@@ -17,11 +18,68 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAll(t *testing.T) {
-
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
 	if !slices.Equal(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
+}
+
+func BenchmarkSum(b *testing.B) {
+	for b.Loop() {
+		Sum([]int{1, 3, 4})
+	}
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for b.Loop() {
+		SumAll([]int{1, 3, 4}, []int{2, 3, 1})
+	}
+}
+
+func BenchmarkSumAllTails(b *testing.B) {
+	for b.Loop() {
+		SumAllTails([]int{1, 3, 4}, []int{2, 3, 1})
+	}
+}
+
+func ExampleSum() {
+	result := Sum([]int{1, 3, 4})
+	fmt.Println(result)
+	// Output: 8
+}
+
+func ExampleSumAll() {
+	result := SumAll([]int{1, 3, 4}, []int{2, 3, 1})
+	fmt.Println(result)
+	// Output: [8 6]
+}
+
+func ExampleSumAllTails() {
+	result := SumAllTails([]int{1, 3, 4}, []int{2, 3, 1})
+	fmt.Println(result)
+	// Output: [7 4]
 }
