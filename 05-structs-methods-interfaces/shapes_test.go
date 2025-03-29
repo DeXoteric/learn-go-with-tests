3aad7d5aae42b33a5687f247990a90e6088468cb
DeXoteric <dexoteric@gmail.com>
2025-03-29 20:32:27 +0100

    structs-methods-interfaces-done

--- 05-structs-methods-interfaces/shapes_test.go
+++ 05-structs-methods-interfaces/shapes_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -14,22 +17,48 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12.0, Height: 6.0}, hasArea: 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+func BenchmarkArea(b *testing.B) {
+	for b.Loop() {
+		Rectangle{5.0, 2.0}.Area()
+		Circle{10.0}.Area()
+		Triangle{10.0, 2.0}.Area()
+	}
+}
+
+func ExampleRectangle_Area() {
+	result := Rectangle{Width: 5.0, Height: 2.0}.Area()
+	fmt.Println(result)
+	// Output: 10
+}
+
+func ExampleCircle_Area() {
+	result := Circle{Radius: 10.0}.Area()
+	fmt.Println(result)
+	// Output: 314.1592653589793
+}
+
+func ExampleTriangle_Area() {
+	result := Triangle{Base: 10.0, Height: 2.0}.Area()
+	fmt.Println(result)
+	// Output: 10
 }
